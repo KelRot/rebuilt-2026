e5784ba1f0b930ed908d1c6890c8e226127478f8
recepsirin0 <recepsirin1@outlook.com>
2026-01-14 22:18:50 +0300

    lib error fix

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2026-library-lastversion-Imported";
+  public static final String MAVEN_NAME = "2026-library-lastversion";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2026-01-14 13:36:21 GMT";
-  public static final long BUILD_UNIX_TIME = 1768397781724L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "e9a541225855abecc24d5922032038835e645375";
+  public static final String GIT_DATE = "2026-01-14 19:17:48 GMT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-01-14 19:18:06 GMT";
+  public static final long BUILD_UNIX_TIME = 1768418286529L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
