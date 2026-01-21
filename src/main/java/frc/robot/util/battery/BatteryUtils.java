384ebf51d41ac7bacb871041b69cc25517ecc894
recepsirin0 <recepsirin1@outlook.com>
2026-01-21 20:28:51 +0300

    updated deprecated function

--- src/main/java/frc/robot/util/battery/BatteryUtils.java
+++ src/main/java/frc/robot/util/battery/BatteryUtils.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants;
 
 public class BatteryUtils {
@@ -31,7 +32,7 @@ public class BatteryUtils {
 
     public static void scheduleMonitor() {
         if (!monitor.isScheduled()) {
-            monitor.schedule();
+            CommandScheduler.getInstance().schedule(monitor);
         }
     }
 }
