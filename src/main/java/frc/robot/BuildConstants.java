474c1b19c4ed4a377bd77e185bfd1d4e819b63c8
Zeynep Afra Yılmaz <zeynepafrayilmaz07@gmail.com>
2026-01-28 20:03:34 +0300

    build updated

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2026-library-lastversion";
+  public static final String MAVEN_NAME = "rebuilt-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "e5784ba1f0b930ed908d1c6890c8e226127478f8";
-  public static final String GIT_DATE = "2026-01-14 19:18:50 GMT";
+  public static final int GIT_REVISION = 22;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "2026-01-26 01:00:48 GMT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-14 19:29:47 GMT";
-  public static final long BUILD_UNIX_TIME = 1768418987873L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-01-28 16:56:09 GMT";
+  public static final long BUILD_UNIX_TIME = 1769619369670L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
