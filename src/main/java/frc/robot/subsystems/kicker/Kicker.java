b7eaef859e64d12ba3d6a93bbc274661027e8c20
EgeCmnn <e.gecumen32@gmail.com>
2026-02-03 11:01:03 +0300

    Add enabled ad

--- src/main/java/frc/robot/subsystems/kicker/Kicker.java
+++ src/main/java/frc/robot/subsystems/kicker/Kicker.java
@@ -11,6 +11,13 @@ import frc.robot.subsystems.kicker.KickerIO.KickerIOInputs;
 
 public class Kicker extends SubsystemBase {
 
+  public enum IndexMode {
+        KICKER_ENABLED,
+        KICKER_DISABLED,
+    }   
+
+  private IndexMode currentMode = IndexMode.KICKER_DISABLED;
+
   private final KickerIO io;
   private final KickerIOInputsAutoLogged inputs = new KickerIOInputsAutoLogged();
 
@@ -18,9 +25,31 @@ public class Kicker extends SubsystemBase {
     this.io = io;
   }
 
+
+  private void checkMode() {
+    if (inputs.rollerMotorCurrentAmps >= Constants.KickerConstants.rollerAmpsLimit) {
+      currentMode = IndexMode.KICKER_ENABLED;
+    } else {
+      currentMode = IndexMode.KICKER_DISABLED;
+    }
+  }
+
   public void setRollerVoltage(double volts) {
     io.setRollerVoltage(volts);
   }
+
+  private void runStateMachine() {
+  switch (currentMode) {
+
+    case KICKER_DISABLED:
+      setRollerVoltage(0.0);
+      break;
+
+    case KICKER_ENABLED:
+      setRollerVoltage(Constants.KickerConstants.defaultRollerVoltage);
+      break;
+  }
+}
 
   public Command setRollerVoltageCommand(double volts) {
     return this.runEnd(() -> setRollerVoltage(volts), () -> setRollerVoltage(0.0));
@@ -29,6 +58,8 @@ public class Kicker extends SubsystemBase {
   @Override
   public void periodic() {
     io.updateInputs(inputs);
+    checkMode();
+    runStateMachine();
     Logger.processInputs("Kicker", inputs);
   }
 
