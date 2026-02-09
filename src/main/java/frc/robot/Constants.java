d60f50c4a97c118c9ac37c9f653be8b1c97a552b
recepsirin0 <recepsirin1@outlook.com>
2026-02-09 15:52:40 +0300

    removed : amp constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,7 +30,6 @@ public final class Constants {
 
     public static class IndexConstants {
         public static final int spinnerMotorID = 3;
-        public static final double spinnerAmpsLimit = 15.0;
 
         public static final double PASSIVE_MODE_VOLTAGE = 3.0;
         public static final double INDEXING_VOLTAGE = 10.0;
