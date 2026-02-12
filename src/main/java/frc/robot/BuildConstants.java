7ecca2ed0c7915165264a9fabad3732439d2de15
recepsirin0 <recepsirin1@outlook.com>
2026-02-12 09:45:49 +0300

    Update BuildConstants and VisionConstants for versioning and camera transforms

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2026-library-lastversion";
+  public static final String MAVEN_NAME = "rebuilt-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "e5784ba1f0b930ed908d1c6890c8e226127478f8";
-  public static final String GIT_DATE = "2026-01-14 19:18:50 GMT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-14 19:29:47 GMT";
-  public static final long BUILD_UNIX_TIME = 1768418987873L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 28;
+  public static final String GIT_SHA = "e8e823e5e16e5692c96f37c57748e45bd6d15a76";
+  public static final String GIT_DATE = "2026-02-11 16:28:26 GMT";
+  public static final String GIT_BRANCH = "Vision";
+  public static final String BUILD_DATE = "2026-02-12 06:35:07 GMT";
+  public static final long BUILD_UNIX_TIME = 1770878107151L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -36,8 +36,8 @@ public class VisionConstants {
 
     // Robot to camera transforms
     // (Not used by Limelight, configure in web UI instead)
-    public static Transform3d robotToCamera0 = new Transform3d(-0.315, -0.01, 0.1, new Rotation3d(0.0, -0.4, -Math.PI/2));
-    public static Transform3d robotToCamera1 = new Transform3d(0, 0.3, 0.10, new Rotation3d(0.0, 0.0, 0));
+    public static Transform3d robotToCamera0 = new Transform3d(-0.02, -16.5, 0.15, new Rotation3d(0.0, 0.0, Math.PI));
+    public static Transform3d robotToCamera1 = new Transform3d(0, 0.3, 0.15, new Rotation3d(0.0, 0.0, 0));
 
     // Basic filtering thresholds
     public static double maxAmbiguity = 0.3; // -29,5
