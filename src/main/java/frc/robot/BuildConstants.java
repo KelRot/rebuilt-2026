e4c462fc5022ef7ba6071b56078fd4ef87ef7e54
EgeCmnn <e.gecumen32@gmail.com>
2026-02-23 11:04:51 +0300

    feat: update turret positioning logic for alliance zones based on robot's Y position

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,6 +7,7 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "rebuilt-2026";
   public static final String VERSION = "unspecified";
+
   public static final int GIT_REVISION = 63;
   public static final String GIT_SHA = "9b24f8ba6d714fdf180b9cb0ff1ca1c3ef76daf3";
   public static final String GIT_DATE = "2026-03-04 06:28:29 GMT";
