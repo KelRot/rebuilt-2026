9b24f8ba6d714fdf180b9cb0ff1ca1c3ef76daf3
eren61q <EgeCmnn>
2026-03-04 09:28:29 +0300

    superstructure error fix

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "rebuilt-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 61;
-  public static final String GIT_SHA = "88e33b7e452b30657b289c2d0c71842089ba9834";
-  public static final String GIT_DATE = "2026-03-04 05:50:59 GMT";
+  public static final int GIT_REVISION = 62;
+  public static final String GIT_SHA = "b2daebee080ea79d57f7a5cf99584085d1b36f49";
+  public static final String GIT_DATE = "2026-03-04 06:08:09 GMT";
   public static final String GIT_BRANCH = "state";
-  public static final String BUILD_DATE = "2026-03-04 06:07:29 GMT";
-  public static final long BUILD_UNIX_TIME = 1772604449320L;
+  public static final String BUILD_DATE = "2026-03-04 06:25:47 GMT";
+  public static final long BUILD_UNIX_TIME = 1772605547263L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -148,7 +149,7 @@ public class Superstructure extends SubsystemBase {
             case DEFAULT:
                 turret.requestState(Turret.SystemState.TRACKING);
                 break;
-
+            
             case IDLE:
             default:
                 stopAll();
@@ -237,6 +238,7 @@ public class Superstructure extends SubsystemBase {
             wantedState = SuperstructureState.TESTING;
         }, this);
     }
+    @Logged
     public SuperstructureState getCurrentState(){
         return currentState;
     }
