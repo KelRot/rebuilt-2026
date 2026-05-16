204fac6a5702d72ec2808ef3251ebbec6a537116
eren61q <EgeCmnn>
2026-05-16 16:43:58 +0300

    under trench auto alıgn deleted

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -71,25 +71,13 @@ public class DriveCommands {
                             ? drive.getRotation().plus(new Rotation2d(Math.PI))
                             : drive.getRotation();
 
-                    if (isInTrenchArea(drive)) {
-                        double ySpeed = MathUtil.clamp(
-                                yController.calculate(drive.getPose().getY(), getTrenchAreaY(drive)),
-                                -drive.getMaxLinearSpeedMetersPerSec(),
-                                drive.getMaxLinearSpeedMetersPerSec());
-                        drive.runVelocity(ChassisSpeeds.fromFieldRelativeSpeeds(
-                                new ChassisSpeeds(
-                                        linearVelocity.getX() * drive.getMaxLinearSpeedMetersPerSec(),
-                                        -ySpeed,
-                                        omega * drive.getMaxAngularSpeedRadPerSec()),
-                                fieldRelativeRotation));
-                    } else {
                         drive.runVelocity(ChassisSpeeds.fromFieldRelativeSpeeds(
                                 new ChassisSpeeds(
                                         linearVelocity.getX() * drive.getMaxLinearSpeedMetersPerSec(),
                                         linearVelocity.getY() * drive.getMaxLinearSpeedMetersPerSec(),
                                         omega * drive.getMaxAngularSpeedRadPerSec()),
                                 fieldRelativeRotation));
-                    }
+                    
                 },
                 drive);
     }
@@ -211,31 +199,4 @@ public class DriveCommands {
         double gyroDelta = 0.0;
     }
 
-    private static Pose2d getBotPoseWithIntake(Drive drive) {
-        Pose2d currentPose = drive.getPose();
-        return new Pose2d(
-                currentPose.getX(),
-                currentPose.getY() + Constants.IntakeConstants.intakeLength / 2.0,
-                currentPose.getRotation());
-    }
-
-    private static boolean isInTrenchArea(Drive drive) {
-        Pose2d botPose = getBotPoseWithIntake(drive);
-        return PointInPolygon.pointInPolygon(botPose, PointInPolygon.blueUpperTrench)
-                || PointInPolygon.pointInPolygon(botPose, PointInPolygon.blueLowerTrench)
-                || PointInPolygon.pointInPolygon(botPose, PointInPolygon.redUpperTrench)
-                || PointInPolygon.pointInPolygon(botPose, PointInPolygon.redLowerTrench);
-    }
-
-    private static double getTrenchAreaY(Drive drive) {
-        Pose2d botPose = getBotPoseWithIntake(drive);
-        if (PointInPolygon.pointInPolygon(botPose, PointInPolygon.blueUpperTrench)
-                || PointInPolygon.pointInPolygon(botPose, PointInPolygon.redUpperTrench)) {
-            return (Field.LeftBlueTrench.openingTopLeft.getY() + Field.LeftBlueTrench.openingTopRight.getY()) / 2.0;
-        } else if (PointInPolygon.pointInPolygon(botPose, PointInPolygon.blueLowerTrench)
-                || PointInPolygon.pointInPolygon(botPose, PointInPolygon.redLowerTrench)) {
-            return (Field.RightBlueTrench.openingTopLeft.getY() + Field.RightBlueTrench.openingTopRight.getY()) / 2.0;
-        }
-        return botPose.getY();
-    }
 }
